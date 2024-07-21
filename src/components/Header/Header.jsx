ea52eeb46b01476f87abc875c8330cb3ad872938
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-07-21 02:46:42 -0700

    feat(Header.jsx): create Header component and import FirstNav and SecondNav component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import FirstNav from "./FirstNav"
+import SecondNav from "./SecondNav"
+const Header = () =>{
+    return(
+        <>
+        <FirstNav/>
+        <SecondNav/>
+        
+        </>
+    )
+}
+
+export default Header
