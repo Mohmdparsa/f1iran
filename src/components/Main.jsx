22bf5aa783edf4c94ed705abd0401c0cb7ac3a06
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-07-21 23:41:01 -0700

    chore(Main.jsx): import Sidebars component and telegram image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,16 @@
-import Header from "./Header/Header"
-import ImageGallery from "./ImageGallery"
-const Main = () =>{
-    return(
-        <>
-        <Header/>
-        <ImageGallery/> 
-        
-        </>
-    )
-}
+import Header from "./Header/Header";
+import ImageGallery from "./ImageGallery";
+import Sidebars from "./MainContent/Sidebars";
+import telegram from "../assets/telegram.png";
+const Main = () => {
+  return (
+    <>
+      <Header />
+      <ImageGallery />
+      <Sidebars />
+      <img src={telegram} alt="" className="mr-[5rem]" />
+    </>
+  );
+};
 
-export default Main
+export default Main;
